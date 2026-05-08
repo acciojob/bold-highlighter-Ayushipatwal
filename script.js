e2e0c9d27a64f1a81c40815484887ba288366c73
Ayushi Patwal <ayushipatwal03@gmail.com>
2026-05-08 15:13:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 function highlight() {
-  const boldWords = document.querySelectorAll("strong");
+    let boldWords = document.querySelectorAll("strong");
 
-  boldWords.forEach(word => {
-    word.style.color = "rgb(0, 128, 0)";
-  });
+    boldWords.forEach(function(word) {
+        word.style.color = "rgb(0, 128, 0)";
+    });
 }
 
 function return_normal() {
-  const boldWords = document.querySelectorAll("strong");
+    let boldWords = document.querySelectorAll("strong");
 
-  boldWords.forEach(word => {
-    word.style.color = "rgb(0, 0, 0)";
-  });
-}
-
-    
+    boldWords.forEach(function(word) {
+        word.style.color = "rgb(0, 0, 0)";
+    });
 }
